f45d611a6ad687975fe7adbace6dca32ff28f7cb
Acaymo Granado Sánchez <acaimopcgc@gmail.com>
2025-12-24 16:39:30 +0000

    clean multiview

--- matlab/create_gaussians.m
+++
@@ -1,7 +0,0 @@
-function G = create_gaussians()
-    % x y z scale r g b opacity
-    % Inicialización aleatoria DIFERENTE al GT
-    G = [
-        0.1  0.1  0.1   0.05   0.5 0.5 0.5   0.5
-    ];
-end
